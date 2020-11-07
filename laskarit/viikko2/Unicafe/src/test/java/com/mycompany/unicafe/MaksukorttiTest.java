474dd831fae5e1e5567ffc7f10dd5a6a438a3b94
Marika Sirkka <marika.sirkka@helsinki.fi>
2020-11-07 17:18:27 +0200

    .gitignore ja MaksukorttiTest

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -10,11 +10,44 @@ public class MaksukorttiTest {
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(1000);
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+    
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+    
+    @Test
+    public void rahanLataaminenKasvattaaSaldoa() {
+        kortti.lataaRahaa(250);
+        assertEquals("saldo: 12.50", kortti.toString());
+    }
+    
+    @Test
+    public void saldoPieneneeKunRahaaOtetaan() {
+        kortti.otaRahaa(250);
+        assertEquals("saldo: 7.50", kortti.toString());
+    }
+    
+    @Test
+    public void saldoEiMuutuJosRahaaOtetaanYliSaldon() {
+        kortti.otaRahaa(1500);
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaTrueJosOttaminenOnnistuu() {
+        assertEquals(true, kortti.otaRahaa(250));
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaFalseJosOttaminenEiOnnistu() {
+        assertEquals(false, kortti.otaRahaa(1500));
     }
 }
